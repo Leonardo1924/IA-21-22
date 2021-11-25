c5f2234ce22378c86943173d8d329adb6bd3f648
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2021-11-25 21:12:29 +0000

    Copiar de Predicados para Gestão
    Copiei o que estava nos predicados auxiliares para gestão os predicados auxiliares encontra-se sem nada

--- src/gestao.pl
+++ src/gestao.pl
@@ -3,10 +3,6 @@
 :-dynamic (encomendaGerida/7).
 :-style_check(-singleton).
 
-
-
-
-
 encontraEstafetaLivre(IdEstafeta) :-
     estafeta(IdEstafeta, _, _, 'base').
 
@@ -17,11 +13,28 @@ decideVeiculo('Mota', Peso) :-
 decideVeiculo('Carro', Peso) :-
      Peso>=21, Peso<101.
 
+calculaPreco('Bicicleta', Prazo, Preco) :- 
+    Prazo < 24, Preco is 2;
+    Prazo < 48, Preco is 1;
+    Prazo > 48, Preco is 0.50;
+ 
+calculaPreco('Mota', Prazo, Preco) :- 
+    Prazo < 24, Preco is 1,7 * 2;
+    Prazo < 48, Preco is 1,7 *  1;
+    Prazo > 48, Preco is 1,7 * 0.50;    
+
+
+calculaPreco('Carro', Prazo, Preco) :- 
+    Prazo < 24, Preco is 3 * 2;
+    Prazo < 48, Preco is 3 * 1;
+    Prazo > 48, Preco is 3 * 0.50;
+    
 auxiliarId(ListaIds) :- findall(Id, encomenda(Id, _, _, _, _), ListaIds).
 auxiliarPeso(ListaPesos) :- findall(Peso, encomenda(_, Peso, _, _, _), ListaPesos).
 auxiliarVol(ListaVol) :- findall(Vol, encomenda(_, _, Vol, _, _), ListaVol).
 auxiliarPrazo(ListaPrazo) :- findall(Prazo, encomenda(_, _, _, Prazo, _), ListaPrazo).
 auxiliarCliente(ListaClientes) :- findall(Cliente, encomenda(_, _, _, _, Cliente),ListaClientes).
+auxiliarDias(ListaDias) :- findall(Dia,encomenda(_,_,_,_,_,Dia),ListaDias).
 
 listaVeiculos([],[]).
 listaVeiculos([H|Tail],[Veiculo|Result]):- 
@@ -42,3 +55,12 @@ gerirEncomenda([],[],[],[],[],[],[]).
 gerirEncomenda([Id|Ids], [Peso|Pesos], [Vol|Vols], [Prazo|Prazos], [Cliente|Clientes], [Veiculo|Veiculos], [Estaf|Estafs]) :-
     assert(encomendaGerida(Id, Peso, Vol, Prazo, Cliente, Veiculo, Estaf)),
     gerirEncomenda(Ids, Pesos, Vols, Prazos, Clientes, Veiculos, Estafs).
+
+listaPrecos([],[],[]).
+listaPrecos([Veiculo|Veiculos],[Prazo|Prazos],[Preco|Precos]):- 
+    calculaPreco(Veiculo,Prazo,Preco), 
+    listaPrecos(Veiculos,Prazos,Precos).
+
+encontraFreguesia([IdCliente|IdsCliente],[Result|Results]):- 
+    findall(Freguesia ,cliente(IdCliente,_,_,_,_,Freguesia,_),Result).
+    encontraFreguesia(IdsCliente,Results).
