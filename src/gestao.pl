888be52792c4385371e3e44c10a0d610806a96d3
emadias <49567783+emadias@users.noreply.github.com>
2021-11-29 20:08:44 +0000

    Update gestao.pl

--- src/gestao.pl
+++ src/gestao.pl
@@ -10,15 +10,19 @@
 
 gerirEncomendas([],[],[],[],[],[],[],[],[]).
 gerirEncomendas([Id|Ids], [Peso|Pesos], [Vol|Vols], [Prazo|Prazos], [Cliente|Clientes], 
-                [Data|Datas], [Veiculo|Veiculos], [Estaf|Estafs], [Preco|Precos]):-
-    \+ Estaf == 'n/a', 
+                [Data|Datas], [Veiculo|Veiculos], [Estaf|Estafs], [Preco|Precos]):- 
     assert(encomendaGerida(Id, Peso, Vol, Prazo, Cliente, Data, Veiculo, Estaf, Preco)),
-    gerirEncomendas(Ids, Pesos, Vols, Prazos, Clientes, Datas, Veiculos, Estafs, Precos).
-gerirEncomendas([_|Ids], [_|Pesos], [_|Vols], [_|Prazos], [_|Clientes], 
-                [_|Datas], [_|Veiculos], ['n/a'|Estafs], [_|Precos]) :-
-    gerirEncomendas(Ids, Pesos, Vols, Prazos, Clientes, Datas, Veiculos, Estafs, Precos).
+    gerirEncomendas(Ids, Pesos, Vols, Prazos, Clientes, Datas, Veiculos, Estafs,Precos).
+
+classificar_entrega(Id, Nota) :-
+    Nota >= 0,
+    Nota =< 5, 
+    retract(entrega(1, Id, _)),
+    assert(entrega(1, Id, Nota)).
+
 
 gerir:-
+    write('entrei no gerir'),
     entregas_popular,
     auxiliarId(Ids),
     auxiliarPeso(Pesos),
@@ -27,12 +31,9 @@ gerir:-
     auxiliarCliente(Clientes),
     auxiliarData(Datas),
     listaVeiculos(Pesos, Veiculos),
-    listaEstafetas(Estafs),
-    listaPrecos(Veiculos, Prazos, Precos).
-    gerirEncomendas(Ids, Pesos, Vols, Prazos, Clientes, Datas, Veiculos, Estafs, Precos).
-
-classificar_entrega(Id, Nota) :-
-    Nota >= 0,
-    Nota =< 5, 
-    retract(entrega(1, Id, _)),
-    assert(entrega(1, Id, Nota)).
+    listaEstafetas(Ids,Estafs),
+    listaPrecos(Veiculos, Prazos, Precos),
+    write(Estafs), nl,
+    write(Prazos),nl, write(Vols), nl, write(Datas),nl, write(Clientes), nl, write(Precos), nl,
+    gerirEncomendas(Ids, Pesos, Vols, Prazos, Clientes, Datas, Veiculos, Estafs, Precos), 
+    write('sai do gerir').
